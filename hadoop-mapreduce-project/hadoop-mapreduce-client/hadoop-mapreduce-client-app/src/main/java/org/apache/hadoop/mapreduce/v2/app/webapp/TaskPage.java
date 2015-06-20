01f1674893ac20558bdacd4879a4f2fc1776357b
Gera Shegalov <gera@apache.org>
2015-06-20 11:38:21 -0700

    MAPREDUCE-6316. Task Attempt List entries should link to the task overview. (Siqi Li via gera)
    (cherry picked from commit 63d40d528654707b3f56619a4eb20e21cdeadc7e)

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/webapp/TaskPage.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/webapp/TaskPage.java
@@ -28,6 +28,7 @@ import static org.apache.hadoop.yarn.webapp.view.JQueryUI.tableInit;
 import java.util.Collection;
 
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.hadoop.mapreduce.v2.api.records.TaskId;
 import org.apache.hadoop.mapreduce.v2.app.job.TaskAttempt;
 import org.apache.hadoop.mapreduce.v2.app.webapp.dao.TaskAttemptInfo;
 import org.apache.hadoop.mapreduce.v2.util.MRWebAppUtil;
@@ -81,8 +82,9 @@ public class TaskPage extends AppView {
 
         String nodeHttpAddr = ta.getNode();
         String diag = ta.getNote() == null ? "" : ta.getNote();
+        TaskId taskId = attempt.getID().getTaskId();
         attemptsTableData.append("[\"")
-        .append(ta.getId()).append("\",\"")
+        .append(getAttemptId(taskId, ta)).append("\",\"")
         .append(progress).append("\",\"")
         .append(ta.getState().toString()).append("\",\"")
         .append(StringEscapeUtils.escapeJavaScript(
@@ -117,6 +119,10 @@ public class TaskPage extends AppView {
 
     }
 
+    protected String getAttemptId(TaskId taskId, TaskAttemptInfo ta) {
+      return ta.getId();
+    }
+
     protected boolean isValidRequest() {
       return app.getTask() != null;
     }
@@ -149,6 +155,9 @@ public class TaskPage extends AppView {
     //logs column should not filterable (it includes container ID which may pollute searches)
     .append("\n{'aTargets': [ 5 ]")
     .append(", 'bSearchable': false }")
+
+    .append("\n, {'sType':'string', 'aTargets': [ 0 ]")
+    .append(", 'mRender': parseHadoopID }")
 
     .append("\n, {'sType':'numeric', 'aTargets': [ 6, 7")
     .append(" ], 'mRender': renderHadoopDate }")
