e3e060c319a50bff825940c686f72dcb548d0cb0
Kihwal Lee <kihwal@apache.org>
2016-03-01 13:02:05 -0600

    HDFS-9198. Coalesce IBR processing in the NN. Contributed by Daryn Sharp.
    (cherry picked from commit 9fa300ad1f41b913a51c35d9c6c8d13a4208550c)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeManager.java
@@ -1352,7 +1352,7 @@ public class DatanodeManager {
           throw new DisallowedDatanodeException(nodeinfo);
         }
 
-        if (nodeinfo == null || !nodeinfo.isAlive) {
+        if (nodeinfo == null || !nodeinfo.isRegistered()) {
           return new DatanodeCommand[]{RegisterCommand.REGISTER};
         }
 
